465e03b8cd7cea205702911bde043bce58939b21
Chris Imperial <cimperial@gbox.adnu.edu.ph>
2017-03-12 14:14:11 +0800

    validation by chris

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+	validates_presence_of :product_name
+	validates_presence_of :product_code
+	validates_presence_of :retailer_price
+	validates_presence_of :original_price
+	validates_presence_of :product_type
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+	validates_presence_of :product_code
+	validates_presence_of :phone_number
+	validates_presence_of :quantity
+	
 end
