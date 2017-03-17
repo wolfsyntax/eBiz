846d8b738803648c62e857ecf0a7a400cac1481d
Angelo Herras <anherras@gbox.adnu.edu.ph>
2017-03-17 12:23:47 +0800

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+	has_and_belongs_to_many :users
+	has_many :transactions
+	
 	validates_presence_of :product_name
 	validates_presence_of :product_code
 	validates_presence_of :retailer_price
