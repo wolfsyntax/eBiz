db66904e69cc9c383c3aa26556be41c9aada2604
Angelo Herras <anherras@gbox.adnu.edu.ph>
2017-03-17 12:24:48 +0800

    Update transaction.rb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
+	has_and_belongs_to_many :users
+	has_many :products
+	
 	validates_presence_of :product_code
 	validates_presence_of :phone_number
 	validates_presence_of :quantity
