a82927fbbc7928bd1a5b898c150bc6ed1d6b6b5e
wolf syntax <wolfsyntax@yahoo.com>
2017-03-22 22:42:33 +0800

    Repush

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,12 +1,12 @@
 class Announcement < ApplicationRecord
 
 	validates :title, presence: true
-	validates :title, format: {with: /[a-zA-Z0-9]/}
+	validates :title, format: {with: /\A[a-zA-Z0-9]+\z/}
 #	validates :title, format: {with: "/[a-zA-Z0-9]/" }
 
-    validates :content, format: {with: /[a-zA-Z0-9]/}
-    validates :Web_URL, format: {with: /(http|https):\/\/www.([a-zA-Z0-9]|[a-zA-Z0-9]|[0-9]{3}.[0-9]{3}.[0-9]{3})/}
-    validates :author, format: {with: /[A-Za-z]/}
-    validates :attachment, format: {with: /[A-Za-z.]/}
+    validates :content, format: {with: /\A[a-zA-Z0-9]+\z/}
+    validates :Web_URL, format: {with: /(http|https):\/\/www.(\A[a-zA-Z0-9]+\z|\A[a-zA-Z0-9]+\z|[0-9]{3}.[0-9]{3}.[0-9]{3})/}
+    validates :author, format: {with: /\A[A-Za-z]+\z/}
+    validates :attachment, format: {with: /\A[A-Za-z.].(jpg|png|jpeg)+\z/}
 #      t.string  :attachment
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,18 @@ class Product < ApplicationRecord
 	has_many :transactions
 	
 	validates_presence_of :product_name
+	validates :product_name, format: {with: /\A[a-zA-Z]+\z/}
+	
 	validates_presence_of :product_code
+	validates :product_code, format: {with: /\A[a-zA-Z]+\z/}
+	
 	validates_presence_of :retailer_price
+	validates :retailer_price, numericality: true
+	
 	validates_presence_of :original_price
+	validates :original_price, numericality: true
+
 	validates_presence_of :product_type
+	validates :product_type, format: {with: /\A[a-zA-Z]+\z/}
 
 end
