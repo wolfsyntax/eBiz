e52ab06fae2bbe3562f03f5e9bd74345c20af160
wolf syntax <wolfsyntax@yahoo.com>
2017-03-24 01:12:32 +0800

    update announcement UI

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -5,7 +5,7 @@ class Announcement < ApplicationRecord
 #	validates :title, format: {with: "/[a-zA-Z0-9]/" }
 
     validates :content, format: {with: /\A[a-zA-Z0-9]+\z/}
-    validates :Web_URL, format: {with: /(http|https):\/\/www.(\A[a-zA-Z0-9]+\z|\A[a-zA-Z0-9]+\z|[0-9]{3}.[0-9]{3}.[0-9]{3})/}
+ #   validates :Web_URL, format: {with: //}
     validates :author, format: {with: /\A[A-Za-z]+\z/}
     validates :attachment, format: {with: /\A[A-Za-z.].(jpg|png|jpeg)+\z/}
 #      t.string  :attachment
